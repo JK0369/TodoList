fda2e85eb86e59e650fcab6462876399c69ea0c6
김종권 <palatable7@naver.com>
2021-04-28 21:01:56 +0900

    Add LoggedOut RIB file

---
+++ TodoList/LoggedOut/LoggedOutViewController.swift
@@ -0,0 +1,21 @@
+//
+//  LoggedOutViewController.swift
+//  TodoList
+//
+//  Created by 김종권 on 2021/04/28.
+//
+
+import RIBs
+import RxSwift
+import UIKit
+
+protocol LoggedOutPresentableListener: class {
+    // TODO: Declare properties and methods that the view controller can invoke to perform
+    // business logic, such as signIn(). This protocol is implemented by the corresponding
+    // interactor class.
+}
+
+final class LoggedOutViewController: UIViewController, LoggedOutPresentable, LoggedOutViewControllable {
+
+    weak var listener: LoggedOutPresentableListener?
+}
