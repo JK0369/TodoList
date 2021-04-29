c012939a59926504e67b0965a6ce9e250de22da8
김종권 <palatable7@naver.com>
2021-04-29 13:27:44 +0900

    Add UI of LoggedOutViewController

--- TodoList/LoggedOut/LoggedOutViewController.swift
+++ TodoList/LoggedOut/LoggedOutViewController.swift
@@ -18,9 +18,54 @@ protocol LoggedOutPresentableListener: class {
 final class LoggedOutViewController: UIViewController, LoggedOutPresentable, LoggedOutViewControllable {
 
     weak var listener: LoggedOutPresentableListener?
+    let bag = DisposeBag()
+
+    lazy var emailTextField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .line
+        textField.placeholder = "email"
+        view.addSubview(textField)
+        return textField
+    }()
+
+    lazy var passwordTextField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .line
+        textField.placeholder = "password"
+        view.addSubview(textField)
+        return textField
+    }()
+
+    lazy var confirmButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("confirm", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .black
+        view.addSubview(button)
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        setupLayout()
+    }
+
+    private func setupLayout() {
+        emailTextField.snp.makeConstraints {
+            $0.top.equalTo(view).offset(120)
+            $0.leading.trailing.equalTo(view).inset(40)
+            $0.height.equalTo(40)
+        }
+
+        passwordTextField.snp.makeConstraints {
+            $0.top.equalTo(emailTextField.snp.bottom).offset(20)
+            $0.leading.trailing.height.equalTo(emailTextField)
+        }
+
+        confirmButton.snp.makeConstraints {
+            $0.top.equalTo(passwordTextField.snp.bottom).offset(20)
+            $0.leading.trailing.height.equalTo(passwordTextField)
+        }
     }
 }
